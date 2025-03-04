82506416f40fbbe4ceaaba5fc2a8b1ed07fc537e
nathanm4 <nathan.mukku@gmail.com>
2025-03-04 16:16:07 -0500

    added first name search logic to search bar, successfully able to ping backend from clicking on employee name in menu after being filtered

--- src/main/resources/static/app/services/employeeService.js
+++ src/main/resources/static/app/services/employeeService.js
@@ -1,17 +1,17 @@
 // services/employeeService.js
 app.factory('EmployeeService', ['$http', function($http) {
     return {
-        // Fetches the home page message from /home endpoint
         getHomePage: function() {
             return $http.get('/home');
         },
-        // Adds an employee by sending a POST request to /addEmployee
         addEmployee: function(employee) {
             return $http.post('/addEmployee', employee);
         },
-        // Optional: fetch all employees
         getAllEmployees: function() {
             return $http.get('/employees');
+        },
+        getEmployeesByFirstName: function(firstName) {
+            return $http.get('/search?firstName=' + firstName);
         }
     };
 }]);
