e02e7878682df69376ab2f841692c3a754f570da
nathanm4 <nathan.mukku@gmail.com>
2025-03-20 13:56:16 -0400

    made address autocomplete form to work with google api and autofill according boxes

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -2,20 +2,79 @@
 var app = angular.module('employeeApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'home.html',
-      controller: 'HomeController'
-    })
-    .when('/addEmployee', {
-      templateUrl: 'employeeForm.html',
-      controller: 'MainController'
-    })
-    .when('/editEmployee', {
-      templateUrl: 'editEmployee.html',
-      controller: 'EditEmployeeController'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+$routeProvider
+  .when('/', {
+    templateUrl: 'home.html',
+    controller: 'HomeController'
+  })
+  .when('/addEmployee', {
+    templateUrl: 'employeeForm.html',
+    controller: 'MainController'
+  })
+  .when('/editEmployee', {
+    templateUrl: 'editEmployee.html',
+    controller: 'EditEmployeeController'
+  })
+  // New route for the address autocomplete form
+  .when('/autoAddress', {
+    templateUrl: 'autoAddressForm.html'
+    // Optionally, you could assign a controller if needed.
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
 }]);
+
+// Custom Directive for Google Places Autocomplete
+app.directive('googlePlacesAutocomplete', function() {
+return {
+  restrict: 'A',
+  link: function(scope, element, attrs) {
+    if (typeof google !== 'undefined' && google.maps && google.maps.places) {
+      var autocomplete = new google.maps.places.Autocomplete(element[0], {
+        types: ['geocode'], // Restrict predictions to addresses.
+        componentRestrictions: { country: 'us' } // Optional: restrict to the US.
+      });
+      autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        var street = "";
+        var city = "";
+        var state = "";
+        var zip = "";
+        var country = "";
+        if (place.address_components) {
+          place.address_components.forEach(function(component) {
+            var componentType = component.types[0];
+            switch (componentType) {
+              case "street_number":
+                street = component.long_name + " ";
+                break;
+              case "route":
+                street += component.long_name;
+                break;
+              case "locality":
+                city = component.long_name;
+                break;
+              case "administrative_area_level_1":
+                state = component.short_name;
+                break;
+              case "postal_code":
+                zip = component.long_name;
+                break;
+              case "country":
+                country = component.long_name;
+                break;
+            }
+          });
+        }
+        // Populate the corresponding fields by their IDs.
+        document.getElementById('street').value = street;
+        document.getElementById('city').value = city;
+        document.getElementById('state').value = state;
+        document.getElementById('zip').value = zip;
+        document.getElementById('country').value = country;
+      });
+    }
+  }
+};
+});
--- src/main/resources/static/app/controllers/searchController.js
+++ src/main/resources/static/app/controllers/searchController.js
@@ -11,7 +11,7 @@ angular.module('employeeApp').controller('SearchController', ['$scope', '$rootSc
         console.error("Error fetching employees", error);
       });
       
-      // Listen for the broadcast to update the employees list live
+      // Listen for live updates to employee list
       $scope.$on('updateEmployees', function(event, data) {
         $scope.employees = data;
       });
@@ -19,6 +19,24 @@ angular.module('employeeApp').controller('SearchController', ['$scope', '$rootSc
       // When an employee is clicked, store the employee in $rootScope
       $scope.selectEmployee = function(employee) {
         $rootScope.selectedEmployee = angular.copy(employee);
+        // Clear the search query so dropdown hides after selection
+        $scope.searchQuery = "";
       };
+  
+      // Close the dropdown when clicking outside of the search card
+      var handler = function(event) {
+        // If the clicked element is not inside an element with class "search-card", clear searchQuery
+        if (!angular.element(event.target).closest('.search-card').length) {
+          $scope.$apply(function() {
+            $scope.searchQuery = "";
+          });
+        }
+      };
+      angular.element(document).on('click', handler);
+  
+      // Clean up the event listener when the controller is destroyed
+      $scope.$on('$destroy', function() {
+        angular.element(document).off('click', handler);
+      });
   }]);
   
